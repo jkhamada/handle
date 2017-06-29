ab0bd1829dcdc10d95879903b814fcd3616314e2
yesyesyo_sk <stevekim.0730@gmail.com>
2017-06-28 20:24:39 -0700

    Login/Registration

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,6 @@
+class DashboardsController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :aliass
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :name, presence: true
+  validates :handle, presence: true, length: { minimum: 7 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  validates :password, presence: true, on: :create
+  before_save :email_lowercase
+
+  def email_lowercase
+      email.downcase!
+  end
 end
